0109d8dee2183b6413751a608d4a37edc71b23b4
Saron Hailemeskel <saronhailemeskel15@gmail.com>
2026-09-01 02:27:02 +0300

    feat: add product creation endpoint

---
+++ controllers/productController.js
@@ -0,0 +1,13 @@
+import Product from "../models/Product.js";
+
+export const createProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
