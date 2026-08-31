97a2a2f80459225c70f1c81d9a857fc39f561607
Saron Hailemeskel <saronhailemeskel15@gmail.com>
2026-09-01 01:09:24 +0300

    feat: add product mongoose model

---
+++ models/Product.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    description: {
+      type: String,
+    },
+
+    category: {
+      type: String,
+    },
+
+    inStock: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
