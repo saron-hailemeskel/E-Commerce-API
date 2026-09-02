cbf1cfb1d686bf7852aa2ca238f26af3152de7bf
Saron Hailemeskel <saronhailemeskel15@gmail.com>
2026-09-02 11:24:16 +0300

    feat: add user login endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,3 +34,42 @@ export const registerUser = async (req, res) => {
     });
   }
 };
+// LOGIN
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+      });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    if (user.password !== password) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    res.status(200).json({
+      message: "Login successful",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
